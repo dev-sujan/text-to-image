191485e6e43e1bd144739187542c16a552e08cb9
dev.sujanmaji@gmail.com <dev.sujanmaji@gmail.com>
2023-01-10 23:22:48 +0530

    .

--- frontend/src/components/ImageContainer.jsx
+++ frontend/src/components/ImageContainer.jsx
@@ -5,20 +5,16 @@ import Image from "./Image";
 
 function ImageContainer({ imgUrl }) {
   return (
-    <div
-      //  Only display container if imgUrl prop is not null
-      className={
-        imgUrl === null
-          ? "hidden"
-          : "bg-gray-400 flex flex-col items-center py-2 shadow-md"
-      }
-    >
-      {/* Display image with the Image component */}
-      <Image imageUrl={imgUrl} />
+    //  Only display container if imgUrl prop is not null
+    imgUrl && (
+      <div className={"bg-gray-400 flex flex-col items-center py-2 shadow-md"}>
+        {/* Display image with the Image component */}
+        <Image imageUrl={imgUrl} />
 
-      {/* Display download button with the DownloadBtn component */}
-      <DownloadBtn downloadImage={() => downloadImg(imgUrl)} />
-    </div>
+        {/* Display download button with the DownloadBtn component */}
+        <DownloadBtn downloadImage={() => downloadImg(imgUrl)} />
+      </div>
+    )
   );
 }
 
