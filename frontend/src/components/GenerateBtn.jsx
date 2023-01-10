35fcf90190748893aba96a2b88e279a1d7513969
dev.sujanmaji@gmail.com <dev.sujanmaji@gmail.com>
2023-01-10 18:46:19 +0530

    .

--- frontend/src/components/GenerateBtn.jsx
+++ frontend/src/components/GenerateBtn.jsx
@@ -9,7 +9,7 @@ function GenerateBtn({ generateHandler }) {
       onClick={generateHandler}
       //  These are the styles applied to the button
       className={
-        "my-2 bg-zinc-800 hover:bg-gray-700 text-[#ff0] font-bold py-2 px-4 border-[#ffff00] border-2 rounded-md shadow-sm text-lg"
+        "my-2 bg-[#ff0] hover:bg-zinc-800 hover:text-[#ff0] text-[#000] text- font-bold py-2 px-4 hover:border-[#ff0] border-[#000] border-2 rounded-md shadow-sm text-lg"
       }
     >
       {/* This is the text displayed on the button */}
